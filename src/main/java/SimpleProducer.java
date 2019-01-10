66a122e44470f157c473db7fcdeee4df312838ce
Nguyen-Cong-Nghiem <40155344+Nguyen-Cong-Nghiem@users.noreply.github.com>
2019-01-10 21:25:48 +0700

    add files project

---
+++ src/main/java/SimpleProducer.java
@@ -0,0 +1,27 @@
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
+public class SimpleProducer {
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.ACKS_CONFIG, "all");
+        props.put(ProducerConfig.RETRIES_CONFIG, 0);
+        props.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
+        props.put(ProducerConfig.LINGER_MS_CONFIG, 1);
+        props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        Producer<String, String> producer = new KafkaProducer<String, String>(props);
+        String topic = "test";
+        for (int i = 0; i < 10; i++) {
+            producer.send(new ProducerRecord<String, String>(topic, "key" + i, "value" + i));
+        }
+        System.out.println("Message sent successfully");
+        producer.close();
+    }
+}
